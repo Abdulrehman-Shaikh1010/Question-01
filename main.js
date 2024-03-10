4cdc9574adaa4c3c1b0369c3b3baee4335eff650
Abdulrehman <you@example.com>
2024-03-10 00:47:21 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var personname = "Hello Eric ,would you learn some python today";
+console.log(personname);
+export {};
---
+++ main.ts
@@ -0,0 +1,3 @@
+var personname : string= "Hello Eric ,would you learn some python today";
+
+console.log(personname);
